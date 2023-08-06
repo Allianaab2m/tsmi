f386333b15c8a101cf2232a985613f1d3ff9bec6
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2023-08-06 22:17:56 +0900

    BREAKING CHANGE: Fixed multiple WebSocket connections

--- src/client.ts
+++ src/client.ts
@@ -3,13 +3,11 @@ import NoteManager from "./manager/note";
 import UserManager from "./manager/user";
 import ReactionManager from "./manager/reaction";
 import TypedEventEmitter from "./eventemitter";
-//import { WsReconnect } from "websocket-reconnect";
-import ReconnectingWebSocket from "reconnecting-websocket";
-import { WebSocketChannels, webSocketHandler } from "./websocket";
+import Stream from "./stream";
+import { WebSocketChannels } from "./stream";
 import { INote } from "./models/note";
 import { IUserLite } from "./models/user";
 import { AnyNotification, MentionNotification } from "./models/notification";
-import ws from "ws";
 
 type ClientEventTypes = {
   ready: [me: Client];
@@ -31,47 +29,21 @@ type ClientEventTypes = {
 type ClientConfig = {
   host: string;
   token: string;
-  channels: Array<WebSocketChannels>;
 };
 
 export default class Client extends TypedEventEmitter<ClientEventTypes> {
   public note: NoteManager;
   public user: UserManager;
   public reaction: ReactionManager;
-  //public websocket: WsReconnect;
-  public websocket: ReconnectingWebSocket;
+  public stream: Stream;
 
   constructor(private config: ClientConfig) {
     super();
-    const session = new HTTPClient(config.host, config.token);
+    const session = new HTTPClient(this.config.host, this.config.token);
     this.user = new UserManager(session, this);
     this.note = new NoteManager(session, this);
     this.reaction = new ReactionManager(session, this);
     // this.websocket = new WsReconnect({ reconnectDelay: 5000 });
-    this.websocket = new ReconnectingWebSocket(
-      `${this.config.host.replace("https", "wss")}?i=${this.config.token}`,
-      [],
-      {
-        WebSocket: ws,
-        minReconnectionDelay: 1000,
-      },
-    );
-  }
-
-  public login() {
-    /* this.websocket.open(
-      `${this.config.host.replace("https", "wss")}?i=${this.config.token}`,
-    ); */
-    let login = false;
-
-    this.websocket.addEventListener("open", () => {
-      if (!login) {
-        this.emit("ready", this);
-        login = true;
-      }
-      webSocketHandler(this.config.channels, this.websocket, this);
-    });
-
-    //webSocketHandler(this.config.channels, this.websocket, this);
+    this.stream = new Stream(this, this.config.host, this.config.token);
   }
 }
